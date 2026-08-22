7e3e27427567780c85485bb3ba64618feef8ddb4
yandri <yandri@gmail.com>
2026-08-22 13:59:48 +0700

    update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Dashboard Admin') - Universitas Metamedia</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/metamedia.png') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/metamedia.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('assets/metamedia.png') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700,800" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', ' Repository Kampus') - Universitas Metamedia</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/metamedia.png') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/metamedia.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('assets/metamedia.png') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link
         href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700,800|fraunces:500,600,700&display=swap"
